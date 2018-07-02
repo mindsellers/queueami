bfad139ace6e83447aafcb562221fdd6ecfa2efc
MindSellers <79063448810@yandex.ru>
2018-07-02 11:42:16 +0400

    Update queueami.py

--- queueami.py
+++ queueami.py
@@ -1,7 +1,7 @@
-
 class queue(object):
     try:
         from ami import AMIClient, SimpleAction, EventListener
+	import random
     except:
         print 'You need to install asterisk-ami (pip install asterisk-ami) first!'
         exit()
@@ -16,28 +16,29 @@ class queue(object):
         self.queue = queue
         self.client = self.AMIClient(address=address, port=int(port))
         self.client.login(username=login, secret=secret)
-        self.action = self.SimpleAction('QueueStatus', Queue=self.queue, ActionID=997700)
+	self.actID=str(self.random.randint(10000,30000))
+        self.action = self.SimpleAction('QueueStatus', Queue=self.queue, ActionID=self.actID)
         self.client.add_event_listener(self.event_QueueMember, white_list=['QueueMember'])
 	self.client.add_event_listener(self.event_QueueStatusComplete, white_list=['QueueStatusComplete'])
         self.resp = self.client.send_action(self.action)
         self.sleep(0.01)
 	while not self.complited:
-	    pass
+	    sleep(0.01)
         self.client.logoff()
         self.free()
 	self.agents=len(self.members)
 	
 
     def event_QueueStatusComplete(self,event,**kwargs):
-	if event.keys['ActionID']=='997700': self.complited=True
+	if event.keys['ActionID']==self.actID: self.complited=True
 
     def event_QueueMember(self, event, **kwargs):
         self.members.append(event.keys)
 
     def free(self):
-        self.freeagents = 0
+	self.freeagents = 0
         for member in self.members:
-	    if member['Status'] == '1' and member['InCall']=='0' and member['Paused']== '0' and member['ActionID']=='997700':
+	    if member['Status'] == '1' and member['InCall']=='0' and member['Paused']== '0' and member['ActionID']==self.actID:
 		
                 self.freeagents += 1
 	return self.freeagents
@@ -47,6 +48,7 @@ class queue(object):
 
 if __name__ == '__main__':
     import sys
+    
     if len(sys.argv) == 6:
 	q=queue(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
 	print q.agents
@@ -54,4 +56,3 @@ if __name__ == '__main__':
         q=None
     else:
         print 'Usage: python queueami.py ami_login ami_pass ami_host ami_port queue'
-
